8f42d953957237fc9fd638903a867dd93d6541fa
Ruxuge <filipwerra.12@gmail.com>
2020-12-14 16:25:43 +0100

    create repo

---
+++ cmake-build-debug/CMakeFiles/bsi_coder.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bsi_coder.dir/main.cpp.o"
+  "bsi_coder"
+  "bsi_coder.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bsi_coder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/bsi_coder.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for bsi_coder.
+# This may be replaced when dependencies are built.
